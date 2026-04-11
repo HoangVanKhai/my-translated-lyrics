5c8a81fb6dd822f67b7b235315b20f728ca97f17
khai96_ <hvksmr1996@gmail.com>
2026-04-12 01:33:32 +0700

    docs: rewording

--- src/install_local_lyrics.rs
+++ src/install_local_lyrics.rs
@@ -27,8 +27,8 @@ const VIDEO_CONFIG_FILENAME: &str = "video.toml";
 #[derive(Deserialize)]
 struct VideoDesc {
     collection: Collection,
-    /// Title of the video this subtitle set translates. Used as
-    /// the stem of target subtitle filenames.
+    /// Title of the video to which this subtitle set apply.
+    /// It is used as the stem of target subtitle filenames.
     video_title: VideoTitle,
     /// Titles of the song in each supported language.
     #[serde(rename = "song-titles")]
